c278179b855cd653f5c52951e38a37041de31e11
mikrob <mikael.robert@botsunit.com>
2017-03-10 19:14:01 +0100

    read openstack config from yaml and list containers

---
+++ main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"bytes"
+	"flag"
+	"fmt"
+	"github.com/ncw/swift"
+	"github.com/spf13/viper"
+	"io/ioutil"
+)
+
+var (
+	kubeconfig = flag.String("c", "./config.yml", "absolute path to the yaml config file for openstack")
+)
+
+func main() {
+	viper.SetConfigType("yaml")
+
+	data, err := ioutil.ReadFile("config.yml")
+	if err != nil {
+		panic("Cannot read config")
+	}
+
+	viper.ReadConfig(bytes.NewBuffer(data))
+
+	if viper.Get("openstack") == nil {
+		panic("Cannot found openstack in config")
+	}
+	username := viper.GetString("openstack.username")
+	api_key := viper.GetString("openstack.api_key")
+	auth_url := viper.GetString("openstack.auth_url")
+	authtenant_name := viper.GetString("openstack.authtenant_name")
+	region := viper.GetString("openstack.region")
+
+	// Create a connection
+	c := swift.Connection{
+		UserName: username,
+		ApiKey:   api_key,
+		AuthUrl:  auth_url,
+		//Domain:   "domain", // Name of the domain (v3 auth only)
+		Tenant: authtenant_name, // Name of the tenant (v2 auth only)
+		Region: region,
+	}
+	// Authenticate
+	errAuth := c.Authenticate()
+	if errAuth != nil {
+		panic(errAuth)
+	}
+	// List all the containers
+	containers, err := c.ContainerNames(nil)
+	fmt.Println(containers)
+}
